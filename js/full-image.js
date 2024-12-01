f884e0bbfa86fa21755b9695f84013d4e795ae1a
Meirbek <53935454+MeirbekAshirbayev@users.noreply.github.com>
2024-12-01 19:43:35 +0500

    fix-1: open and close

--- js/full-image.js
+++ js/full-image.js
@@ -1,6 +1,6 @@
 
 import { dataArrayObjectElement } from './thumbnails.js';
-import { getRandomInteger } from './utils';
+//import { getRandomInteger } from './utils';
 
 const pictures = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
@@ -18,13 +18,15 @@ function makePictureBig (evt) {
   const picture = evt.target.closest('.picture');
 
   if (picture) {
+    evt.preventDefault();
+
     const pictureImg = picture.querySelector('.picture__img');
     const bigPictureImg = bigPicture.querySelector('img');
     const bigPictureLikesCount = bigPicture.querySelector('.likes-count');
     const socialCommentCount = bigPicture.querySelector('.social__comment-count');
     const commentsLoader = bigPicture.querySelector('.comments-loader')
     const socialCommentTotalCount = bigPicture.querySelector('.social__comment-total-count');
-    const socialCommentShownCount = bigPicture.querySelector('.social__comment-shown-count');
+    // const socialCommentShownCount = bigPicture.querySelector('.social__comment-shown-count');
     const socialComments = bigPicture.querySelector('.social__comments');
     const socialComment = socialComments.querySelector('.social__comment');
     const socialCaption = bigPicture.querySelector('.social__caption');
@@ -33,26 +35,28 @@ function makePictureBig (evt) {
     const pictureImgUrl = pictureImg.src;
     const pictureNumber = Number(pictureImgUrl.slice(29, pictureImgUrl.length - 4));
     const pictureObj = dataArrayObjectElement(pictureNumber - 1);
-    const numberCommentStart = getRandomInteger(0, pictureObj.comments.length - 2);
 
     bigPictureImg.src = pictureObj.url;
     bigPictureLikesCount.textContent = pictureObj.likes;
-    socialCommentShownCount.textContent = numberCommentStart + 1;
     socialCommentTotalCount.textContent = pictureObj.comments.length;
     socialCaption.textContent = pictureObj.description;
 
     socialComments.innerHTML = '';
 
-    for (let i = numberCommentStart; i < numberCommentStart + 2; i++) {
+    const pictureCommentsFragment = document.createDocumentFragment();
+
+    for (let i = 0; i < pictureObj.comments.length; i++) {
       const socialCommentClone = socialComment.cloneNode(true);
-      //const socialCommentImage = socialCommentClone.querySelector('img');
+      const socialCommentImage = socialCommentClone.querySelector('img');
       const socialCommentText = socialCommentClone.querySelector('p');
 
-      //socialCommentImage.src = pictureObj.comments[i].url;
+      socialCommentImage.src = pictureObj.comments[i].avatar;
       socialCommentText.textContent = pictureObj.comments[i].message;
 
-      socialComments.appendChild(socialCommentClone);
+      pictureCommentsFragment.appendChild(socialCommentClone);
     }
+
+    socialComments.appendChild(pictureCommentsFragment);
 
     socialCommentCount.classList.add('hidden');
     commentsLoader.classList.add('hidden');
@@ -62,7 +66,6 @@ function makePictureBig (evt) {
 
 
     document.addEventListener('keydown', onDocumentKeydown);
-
   }
 }
 
@@ -77,6 +80,7 @@ pictures.addEventListener('click', (evt) => {
   makePictureBig(evt);
 });
 
-closeBigPictureElement.addEventListener('click', () => {
+closeBigPictureElement.addEventListener('click', (evt) => {
+  evt.preventDefault();
   closeBigPicture();
 });
